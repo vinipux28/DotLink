526c0b319bb5373f0dbe7036e71d66b07a3eb441
VOOleksandrov22 <vooleksandrov22@codingburgas.bg>
2025-10-19 21:18:30 +0300

    Add IPostRepository interface. Closes #2

---
+++ DotLink.Application/Repositories/IPostRepository.cs
@@ -0,0 +1,22 @@
+﻿using DotLink.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DotLink.Application.Repositories
+{
+    public interface IPostRepository
+    {
+        Task<Post?> GetByIdAsync(Guid postId);
+
+        Task<IEnumerable<Post>> GetRecentPostsAsync(int skip, int take);
+
+        Task AddAsync(Post post);
+
+        Task UpdateAsync(Post post);
+
+        Task DeleteAsync(Post post);
+
+        Task<Post?> GetPostWithDetailsAsync(Guid postId);
+    }
+}
