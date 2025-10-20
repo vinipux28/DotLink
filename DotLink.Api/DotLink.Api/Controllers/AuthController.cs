86062650bcba05082483d9d5ec91a3921c6b1ffa
vinipux28 <viniwell2807@gmail.com>
2025-10-20 21:54:12 +0300

    Add AuthController

---
+++ DotLink.Api/DotLink.Api/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using DotLink.Application.Commands.UserCommands;
+using System.Threading.Tasks;
+
+namespace DotLink.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Guid userId = await _mediator.Send(command);
+
+                return CreatedAtAction(nameof(Register), new { id = userId }, new { UserId = userId });
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(new { error = ex.Message });
+            }
+        }
+
+    }
+}
