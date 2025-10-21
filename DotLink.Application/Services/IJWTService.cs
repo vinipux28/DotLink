090094c93acacb533c8c79b61d0e6e04bfa81f28
vinipux28 <viniwell2807@gmail.com>
2025-10-21 21:50:11 +0300

    Minor changes

---
+++ DotLink.Application/Services/IJWTService.cs
@@ -0,0 +1,9 @@
+﻿using DotLink.Domain.Entities;
+
+namespace DotLink.Application.Services
+{
+    public interface IJwtService
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ DotLink.Infrastructure/Services/JWTService.cs
@@ -0,0 +1,53 @@
+﻿using DotLink.Application.Services;
+using DotLink.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System;
+
+namespace DotLink.Infrastructure.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly byte[] _secretKey;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            string secret = _configuration["Jwt:Key"]
+                ?? throw new InvalidOperationException("Jwt:Key was not found in config");
+
+            _secretKey = Encoding.ASCII.GetBytes(secret);
+        }
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var claims = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Username)
+            });
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(_secretKey),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
