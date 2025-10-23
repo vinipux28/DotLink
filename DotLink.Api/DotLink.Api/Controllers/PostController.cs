42a8799854c3e33adafd4135e721b329e472152e
vinipux28 <viniwell2807@gmail.com>
2025-10-23 21:27:24 +0300

    Start implementing PostController

---
+++ DotLink.Api/DotLink.Api/Controllers/PostController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace DotLink.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PostController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost([FromBody] CreatePostCommand command)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userIdClaim == null || !Guid.TryParse(userIdClaim, out Guid userId))
+            {
+                return Unauthorized();
+            }
+
+            command.UserId = userId;
+
+            Guid postId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetPostById), new { id = postId }, new { PostId = postId });
+        }
+
+        /*[HttpGet("{id}")]
+        public IActionResult GetPostById(Guid id)
+        {
+            
+        }*/
+    }
+}
