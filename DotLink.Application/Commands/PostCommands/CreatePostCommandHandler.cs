405cd46a56ebeb480bbdebb070632023ef2ab910
vinipux28 <viniwell2807@gmail.com>
2025-10-23 21:32:39 +0300

    Create commands for creating posts

---
+++ DotLink.Application/Commands/PostCommands/CreatePostCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using DotLink.Application.Repositories;
+using DotLink.Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotLink.Application.Commands.PostCommands
+{
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, Guid>
+    {
+        private readonly IPostRepository _postRepository;
+
+        public CreatePostCommandHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<Guid> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            var newPost = new Post(
+                Guid.NewGuid(),
+                request.UserId,
+                request.Title,
+                request.Content
+            );
+
+            await _postRepository.AddAsync(newPost);
+
+            return newPost.Id;
+        }
+    }
+}
