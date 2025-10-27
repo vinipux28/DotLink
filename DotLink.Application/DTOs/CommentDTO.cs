436f164a3a75a951c626e76af85b44d88bd5df70
vinipux28 <viniwell2807@gmail.com>
2025-10-27 21:21:26 +0200

    Add CommentDTO

---
+++ DotLink.Application/DTOs/CommentDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotLink.Application.DTOs
+{
+    public class CommentDTO
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string AuthorUsername { get; set; }
+    }
+}
