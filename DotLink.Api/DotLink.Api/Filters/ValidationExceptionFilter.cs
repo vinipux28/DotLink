74231c8a8116fc64bd9d6b2cbb1f03955b64a3cb
vinipux28 <viniwell2807@gmail.com>
2025-10-28 21:41:07 +0200

    Add ValidationExceptionFilter class

---
+++ DotLink.Api/DotLink.Api/Filters/ValidationExceptionFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using FluentValidation;
+using System.Net;
+
+namespace DotLink.Api.Filters
+{
+    public class ValidationExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is ValidationException validationException)
+            {
+                context.HttpContext.Response.ContentType = "application/json";
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest; // 400
+
+                var errorResponse = new
+                {
+                    status = (int)HttpStatusCode.BadRequest,
+                    title = "Validation error",
+                    detail = "One or more parameters provided were not valid.",
+                    errors = validationException.Errors
+                        .GroupBy(e => e.PropertyName)
+                        .ToDictionary(
+                            g => g.Key,
+                            g => g.Select(e => e.ErrorMessage).ToArray()
+                        )
+                };
+
+                context.Result = new JsonResult(errorResponse);
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
