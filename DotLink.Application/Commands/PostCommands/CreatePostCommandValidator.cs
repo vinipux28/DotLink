183cccea5c78190fe73291c77085dabab04bea5e
vinipux28 <viniwell2807@gmail.com>
2025-10-28 11:48:03 +0200

    Add CreatePostCommandValidator

---
+++ DotLink.Application/Commands/PostCommands/CreatePostCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace DotLink.Application.Commands.PostCommands
+{
+    public class CreatePostCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreatePostCommandValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(255).WithMessage("Title has to have maximum length of 255 characters.");
+
+            RuleFor(x => x.Content)
+                .NotEmpty().WithMessage("Post content can't be blank.");
+        }
+    }
+}
