39eabea62f54d7f9104b6e9c1a4652a3c822ceab
vinipux28 <viniwell2807@gmail.com>
2025-10-28 11:51:12 +0200

    Add CastVoteCommandValidator

---
+++ DotLink.Application/Commands/PostCommands/CastVoteCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace DotLink.Application.Commands.PostCommands
+{
+    public class CastVoteCommandValidator : AbstractValidator<CastVoteCommand>
+    {
+        public CastVoteCommandValidator() { 
+            RuleFor(x => x.PostId)
+                .NotEmpty().WithMessage("PostId is required.");
+        }
+    }
+}
