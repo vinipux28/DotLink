11f0c6d1c692b8f30cdeb368c254ef9367be737d
vinipux28 <viniwell2807@gmail.com>
2025-11-01 16:06:08 +0200

    Add UpdatePostCommand, its handler and validator

---
+++ DotLink.Application/Commands/PostCommands/UpdatePostCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotLink.Application.Repositories;
+
+namespace DotLink.Application.Commands.PostCommands
+{
+    public class UpdatePostCommandHandler : IRequestHandler<UpdatePostCommand, Unit>
+    {
+        private readonly IPostRepository _postRepository;
+        public UpdatePostCommandHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<Unit> Handle(UpdatePostCommand request, CancellationToken cancellationToken)
+        {
+            var post = await _postRepository.GetByIdAsync(request.PostId);
+
+            if (post is null)
+            {
+                throw new Exception($"Post with ID {request.PostId} not found.");
+            }
+
+            post.UpdateTitle(request.Title);
+            post.UpdateContent(request.Content);
+
+            await _postRepository.UpdateAsync(post);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ DotLink.Application/Commands/PostCommands/UpdatePostCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace DotLink.Application.Commands.PostCommands
+{
+    public class UpdatePostCommandValidator : AbstractValidator<UpdatePostCommand>
+    {
+        public UpdatePostCommandValidator()
+        {
+            RuleFor(x => x.PostId)
+                .NotEmpty().WithMessage("Post ID is required.");
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Post title is required.")
+                .MaximumLength(255).WithMessage("Title has to have maximum length of 22 characters.");
+
+            RuleFor(x => x.Content)
+                .NotEmpty().WithMessage("Post content is required.");
+        }
+    }
+}
