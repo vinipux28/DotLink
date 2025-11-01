edd38149179c9d6ccf91b7b38234f7782b187b76
vinipux28 <viniwell2807@gmail.com>
2025-11-01 16:44:19 +0200

    Add DeletePostCommandValidator class

---
+++ DotLink.Application/Commands/PostCommands/DeletePostCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+
+namespace DotLink.Application.Commands.PostCommands
+{
+    public class DeletePostCommandValidator : AbstractValidator<DeletePostCommand>
+    {
+        public DeletePostCommandValidator()
+        {
+            RuleFor(x => x.PostId)
+                .NotEmpty().WithMessage("Post ID is required.");
+        }
+    }
+}
