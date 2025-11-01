e866e0a9b3ac867fc8a60fd20e1b3ca13a836250
vinipux28 <viniwell2807@gmail.com>
2025-11-01 16:42:23 +0200

    Add DeletePostCommand class

---
+++ DotLink.Application/Commands/PostCommands/DeletePostCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+
+namespace DotLink.Application.Commands.PostCommands
+{
+    public class DeletePostCommand : IRequest<Unit>
+    {
+        public Guid PostId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
