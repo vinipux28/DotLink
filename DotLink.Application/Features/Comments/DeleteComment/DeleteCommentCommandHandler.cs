3c29c1d9bac9b6498f9795da88c893633dc42ebf
vinipux28 <viniwell2807@gmail.com>
2025-11-02 21:22:30 +0200

    Imlement command, handler and validator for deleting comment

---
+++ DotLink.Application/Features/Comments/DeleteComment/DeleteCommentCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using DotLink.Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotLink.Application.Features.Comments.DeleteComment
+{
+    public class DeleteCommentCommandHandler : IRequestHandler<DeleteCommentCommand, Unit>
+    {
+        private readonly ICommentRepository _commentRepository;
+        public DeleteCommentCommandHandler(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+        public async Task<Unit> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+        {
+            // TODO : Call delete comment method from repository
+            await Task.CompletedTask;
+            return Unit.Value;
+        }
+    }
+}
