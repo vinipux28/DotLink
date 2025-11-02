1692df1453a510db6ba39f38a635cbd69011328a
vinipux28 <viniwell2807@gmail.com>
2025-11-02 21:33:04 +0200

    Add UpdateCommentCommandHandler

---
+++ DotLink.Application/Features/Comments/UpdateComment/UpdateCommentCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using DotLink.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotLink.Application.Features.Comments.UpdateComment
+{
+    public class UpdateCommentCommandHandler
+    {
+        private readonly ICommentRepository _commentRepository;
+        public UpdateCommentCommandHandler(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        public async Task Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
+        {
+            var comment = await _commentRepository.GetByIdAsync(request.CommentId);
+            if (comment is null)
+            {
+                throw new Exception($"Comment with ID {request.CommentId} not found.");
+            }
+            comment.UpdateContent(request.Content);
+            await _commentRepository.UpdateAsync(comment);
+        }
+
+    }
+}
