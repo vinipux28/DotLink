4bc373708d65935c8c657860839a7a2dcd7afbd3
vinipux28 <viniwell2807@gmail.com>
2025-11-02 21:45:48 +0200

    Add CommentController with no routes

---
+++ DotLink.Api/DotLink.Api/Controllers/CommentController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotLink.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/Post")]
+    public class CommentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CommentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+    }
+}
