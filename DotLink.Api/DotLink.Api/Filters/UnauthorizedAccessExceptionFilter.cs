1a347ad30903442d48cb868c3634c428d2ec3a40
vinipux28 <viniwell2807@gmail.com>
2025-11-03 21:36:57 +0200

    Add UnauthorizedAccessExceptionFilter

---
+++ DotLink.Api/DotLink.Api/Filters/UnauthorizedAccessExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DotLink.Api.Filters
+{
+    public class UnauthorizedAccessExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is UnauthorizedAccessException)
+            {
+                context.HttpContext.Response.ContentType = "application/json";
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden; // 403
+
+                var errorResponse = new
+                {
+                    status = (int)HttpStatusCode.Forbidden,
+                    title = "Access denied",
+                    detail = context.Exception.Message
+                };
+
+                context.Result = new JsonResult(errorResponse);
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+
+}
+}
