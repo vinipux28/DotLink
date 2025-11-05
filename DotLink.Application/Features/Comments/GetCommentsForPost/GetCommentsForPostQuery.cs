062b664c17b945111d365560b7390d3ae03be96a
vinipux28 <viniwell2807@gmail.com>
2025-11-05 21:04:24 +0200

    Update Query in order to support pagination

--- DotLink.Application/Features/Comments/GetCommentsForPost/GetCommentsForPostQuery.cs
+++ DotLink.Application/Features/Comments/GetCommentsForPost/GetCommentsForPostQuery.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 
 namespace DotLink.Application.Features.Comments.GetCommentsForPost
 {
-    public class GetCommentsForPostQuery : IRequest<List<CommentDTO>>
+    public class GetCommentsForPostQuery : IRequest<PaginatedResponse<CommentDTO>>
     {
         public Guid PostId { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
