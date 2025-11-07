6308d3eed153bd7f1fff7323d9f4fc63b31e7310
vinipux28 <viniwell2807@gmail.com>
2025-11-07 22:10:23 +0200

    Add service interface for file manipulation

---
+++ DotLink.Application/Repositories/IFileStorageService.cs
@@ -0,0 +1,11 @@
+using System.IO;
+
+namespace DotLink.Application.Services
+{
+	public interface IFileStorageService
+	{
+		Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType, string containerName);
+
+		Task DeleteFileAsync(string fileUrl);
+	}
+}
