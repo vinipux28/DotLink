27013137e83e50259c1d998ac9379c09f0412db2
vinipux28 <viniwell2807@gmail.com>
2025-11-07 22:08:16 +0200

    Add UpdateUserProfileCommand with support of file uploading

---
+++ DotLink.Application/Features/Users/UpdateUser/UpdateUserProfileCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+public class UpdateUserProfileCommand : IRequest<Unit>
+{
+    public Guid UserId { get; set; }
+    public string? NewUsername { get; set; }
+    public string? NewBio { get; set; }
+
+    public Stream? ProfilePictureStream { get; set; } // File contents
+    public string? ProfilePictureFileName { get; set; } // File name
+    public string? ProfilePictureContentType { get; set; } // Type (image/jpeg)
+}
