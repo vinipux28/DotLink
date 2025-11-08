f9e7b19c3d426e3269ad58198ca395a6253f4e44
vinipux28 <viniwell2807@gmail.com>
2025-11-08 16:45:06 +0200

    Add GET /api/User for fetching user profile data(excluding profile picture, but with key to it)

---
+++ DotLink.Api/DotLink.Api/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using DotLink.Application.DTOs;
+using DotLink.Application.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotLink.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(new UserDTO(user));
+        }
+    }
+}
