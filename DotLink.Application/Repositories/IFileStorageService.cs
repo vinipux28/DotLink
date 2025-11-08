b1ebae5c7322f40d6495dcaf0665e9c697e6812a
vinipux28 <viniwell2807@gmail.com>
2025-11-08 15:55:20 +0200

    Add GetFileStreamAsync method to IFileStorageService interface

--- DotLink.Application/Repositories/IFileStorageService.cs
+++ DotLink.Application/Repositories/IFileStorageService.cs
@@ -7,5 +7,7 @@ namespace DotLink.Application.Services
 		Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType, string containerName);
 
 		Task DeleteFileAsync(string fileUrl);
+
+		Task<Stream> GetFileStreamAsync(string fileKey);
 	}
 }
