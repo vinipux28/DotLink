4676891cdbfe66c08aa7f06d7eda0cc2a60533b3
vinipux28 <viniwell2807@gmail.com>
2025-11-08 17:32:21 +0200

    Add UploadProfilePictureCommandHandler

--- DotLink.Application/Features/Users/UploadProfilePicture/UploadProfilePictureCommand.cs
+++ DotLink.Application/Features/Users/UploadProfilePicture/UploadProfilePictureCommand.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 namespace DotLink.Application.Features.Users.UploadProfilePicture
 {
-    public class UploadProfilePictureCommand : IRequest<Unit>
+    public class UploadProfilePictureCommand : IRequest<string>
     {
         public Guid UserId { get; set; }
         public Stream ProfilePictureStream { get; set; } = default!;
