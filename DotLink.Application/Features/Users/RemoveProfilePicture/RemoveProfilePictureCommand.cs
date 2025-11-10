87f76c0cf96483bb228bad6d9110abedd8d5df97
vinipux28 <viniwell2807@gmail.com>
2025-11-10 20:41:45 +0200

    Add command class

---
+++ DotLink.Application/Features/Users/RemoveProfilePicture/RemoveProfilePictureCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace DotLink.Application.Features.Users.RemoveProfilePicture
+{
+    public class RemoveProfilePictureCommand : IRequest<Unit>
+    {
+        public Guid UserId { get; set; }
+    }
+}
