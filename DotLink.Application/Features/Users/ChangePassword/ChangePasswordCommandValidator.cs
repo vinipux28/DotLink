e6d18b4ff783ebb902b1de14b4a4d6dad80a6f14
vinipux28 <viniwell2807@gmail.com>
2025-11-10 21:22:00 +0200

    Add ChangePasswordCommand

---
+++ DotLink.Application/Features/Users/ChangePassword/ChangePasswordCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace DotLink.Application.Features.Users.ChangePassword
+{
+    public class ChangePasswordCommandValidator : AbstractValidator<ChangePasswordCommand>
+    {
+        public ChangePasswordCommandValidator()
+        {
+            RuleFor(x => x.OldPassword)
+                .NotEmpty()
+                .WithMessage("Old password is required.");
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty()
+                .MinimumLength(8)
+                .WithMessage("New password has to have minimum length of 8 characters.");
+
+            RuleFor(x => x.NewPassword)
+                .NotEqual(x => x.OldPassword)
+                .WithMessage("New password cannot be the same as the old password.");
+
+            RuleFor(x => x.ConfirmNewPassword)
+                .Equal(x => x.NewPassword)
+                .WithMessage("The new password and confirmation password do not match.");
+        }
+    }
+}
