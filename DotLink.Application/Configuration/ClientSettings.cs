5f8761e625935b110679627b7b6faf364c49af24
vinipux28 <viniwell2807@gmail.com>
2025-11-11 12:58:22 +0200

    Add ClientSettings class for dynamic domain insertion

---
+++ DotLink.Application/Configuration/ClientSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace DotLink.Application.Configuration
+{
+    public class ClientSettings
+    {
+        public const string SectionName = "ClientSettings";
+
+        public string BaseUrl { get; set; } = string.Empty;
+        public string ResetPasswordPath { get; set; } = string.Empty;
+    }
+}
