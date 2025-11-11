ae3d33d1dd3635403270ec9a1002afefd1cd1948
vinipux28 <viniwell2807@gmail.com>
2025-11-11 22:11:48 +0200

    Implement basic CompositeSearchQueryHandler

---
+++ DotLink.Application/Features/Search/CompositeSearchQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using DotLink.Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotLink.Application.Features.Search
+{
+    public class CompositeSearchQueryHandler : IRequestHandler<CompositeSearchQuery, List<SearchResultItem>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPostRepository _postRepository;
+        public CompositeSearchQueryHandler(IUserRepository userRepository, IPostRepository postRepository)
+        {
+            _userRepository = userRepository;
+            _postRepository = postRepository;
+        }
+
+        public async Task<List<SearchResultItem>> Handle(CompositeSearchQuery request, CancellationToken cancellationToken)
+        {
+            var users = _userRepository.SearchUsersAsync(request.SearchTerm);
+            var posts = _postRepository.SearchPostsAsync(request.SearchTerm);
+            await Task.WhenAll(
+                users,
+                posts
+            );
+
+            var combinedResults = users.Result
+                                        .Select(u => new SearchResultItem(u))
+                                        .Concat(
+                                            posts.Result.Select(p => new SearchResultItem(p))
+                                        )
+                                        .OrderByDescending(r => r.RelevanceScore)
+                                        .Take(50)
+                                        .ToList();
+
+            return combinedResults;
+
+        }
+    }
+}
--- DotLink.Application/Features/Search/SearchResultItem.cs
+++ DotLink.Application/Features/Search/SearchResultItem.cs
@@ -15,5 +15,27 @@ namespace DotLink.Application.Features.Search
         string PreviewContent,
         double RelevanceScore,
         string? ImageKey
-    );
+    )
+    {
+        public SearchResultItem(DotLink.Domain.Entities.User user)
+            : this(
+                  user.Id,
+                  EntityType.User,
+                  user.Username,
+                  string.Empty,
+                  1.0,
+                  user.ProfilePictureKey
+              )
+        { }
+        public SearchResultItem(DotLink.Domain.Entities.Post post)
+            : this(
+                  post.Id,
+                  EntityType.Post,
+                  post.Title,
+                  post.Content.Length <= 100 ? post.Content : post.Content.Substring(0, 100) + "...",
+                  1.0,
+                  null
+              )
+        { }
+    };
 }
