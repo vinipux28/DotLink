ba40b156697453eec16f55b933ff99b6f772c04e
vinipux28 <viniwell2807@gmail.com>
2025-11-11 12:54:19 +0200

    Add command handler

---
+++ DotLink.Application/Features/Users/ForgotPassword/ForgotPasswordCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DotLink.Application.Repositories;
+using DotLink.Application.Services;
+
+namespace DotLink.Application.Features.Users.ForgotPassword
+{
+    public class ForgotPasswordCommandHandler : IRequestHandler<ForgotPasswordCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IEmailService _emailService;
+
+        public ForgotPasswordCommandHandler(IUserRepository userRepository, IEmailService emailService)
+        {
+            _userRepository = userRepository;
+            _emailService = emailService;
+        }
+
+        public async Task<Unit> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                await Task.Delay(500, cancellationToken); // mock delay
+                return Unit.Value;
+            }
+
+            string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            DateTime expiry = DateTime.UtcNow.AddHours(1);
+
+            user.SetPasswordResetToken(token, expiry);
+            await _userRepository.UpdateAsync(user);
+
+            string resetLink = $"https://TO-DO/reset-password?token={token}&email={user.Email}";
+
+            await _emailService.SendPasswordResetEmailAsync(user.Email, user.Username, resetLink);
+
+            return Unit.Value;
+        }
+    }
+}
