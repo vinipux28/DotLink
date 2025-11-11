aebf1a481841ae123f81ef018947dd64523dac80
vinipux28 <viniwell2807@gmail.com>
2025-11-11 13:10:49 +0200

    Add ResetPasswordCommandValidator

---
+++ DotLink.Application/Features/Users/ResetPassword/ResetPasswordCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace DotLink.Application.Features.Users.ResetPassword
+{
+    public class ResetPasswordCommandValidator : AbstractValidator<ResetPasswordCommand>
+    {
+        public ResetPasswordCommandValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotEmpty()
+                .WithMessage("Reset token is required.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty()
+                .MinimumLength(8);
+
+            RuleFor(x => x.ConfirmNewPassword)
+                .Equal(x => x.NewPassword)
+                .WithMessage("The new password and confirmation password do not match.");
+        }
+    }
+}
