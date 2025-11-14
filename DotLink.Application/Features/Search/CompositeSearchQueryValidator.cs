9ae71316786d179ededae270becf93bbdcca76cb
vinipux28 <viniwell2807@gmail.com>
2025-11-14 21:33:06 +0200

    Add validator ro CompositeSearchQuery

---
+++ DotLink.Application/Features/Search/CompositeSearchQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotLink.Application.Features.Search
+{
+    public class CompositeSearchQueryValidator : AbstractValidator<CompositeSearchQuery>
+    {
+        public CompositeSearchQueryValidator()
+        {
+            RuleFor(x => x.SearchTerm)
+                .NotEmpty().WithMessage("Search term must not be empty.")
+                .MaximumLength(100).WithMessage("Search term has to have maximum length of 100 characters.");
+        }
+    }
+}
