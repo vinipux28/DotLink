552f4a6e6dc5191ce5db76348d27e8b6d87ccf66
vinipux28 <viniwell2807@gmail.com>
2025-11-15 22:46:31 +0200

    Ensure username is unique on user registration

--- DotLink.Application/Features/Users/RegisterUser/RegisterUserCommandHandler.cs
+++ DotLink.Application/Features/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -20,10 +20,14 @@ namespace DotLink.Application.Features.Users.RegisterUser
         public async Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
         {
             var existingUser = await _userRepository.GetByEmailAsync(request.Email);
-
             if (existingUser != null)
             {
                 throw new DotLinkConflictException("User", "email", request.Email);
+            }
+
+            existingUser = await _userRepository.GetByUsernameAsync(request.Username);
+            if (existingUser != null) {
+                throw new DotLinkConflictException("User", "username", request.Username);
             }
 
             var passwordHash = User.HashPassword(request.Password);
