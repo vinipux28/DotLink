710dc127a78f0a6d56772f098c6e981c358444b0
vinipux28 <viniwell2807@gmail.com>
2025-11-17 21:48:50 +0200

    Update UserRepository in order to support user follwoing

--- DotLink.Application/Repositories/IUserRepository.cs
+++ DotLink.Application/Repositories/IUserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DotLink.Domain.Entities;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace DotLink.Application.Repositories
 {
@@ -13,5 +14,10 @@ namespace DotLink.Application.Repositories
         Task AddAsync(User user);
         Task UpdateAsync(User user);
         Task<bool> IsUsernameUniqueAsync(string username);
+
+        // Follow/Unfollow operations
+        Task FollowAsync(Guid followerId, Guid followeeId);
+        Task UnfollowAsync(Guid followerId, Guid followeeId);
+        Task<bool> IsFollowingAsync(Guid followerId, Guid followeeId);
     }
 }
--- DotLink.Infrastructure/Repositories/UserRepository.cs
+++ DotLink.Infrastructure/Repositories/UserRepository.cs
@@ -65,5 +65,31 @@ namespace DotLink.Infrastructure.Repositories
                 .AsNoTracking()
                 .AnyAsync(u => u.Username == username);
         }
+
+        public async Task FollowAsync(Guid followerId, Guid followeeId)
+        {
+            if (followerId == followeeId) return;
+
+            var exists = await _context.UserFollows.FindAsync(followerId, followeeId);
+            if (exists != null) return;
+
+            var relation = new UserFollow(followerId, followeeId);
+            _context.UserFollows.Add(relation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UnfollowAsync(Guid followerId, Guid followeeId)
+        {
+            var relation = await _context.UserFollows.FindAsync(followerId, followeeId);
+            if (relation == null) return;
+
+            _context.UserFollows.Remove(relation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsFollowingAsync(Guid followerId, Guid followeeId)
+        {
+            return await _context.UserFollows.AnyAsync(uf => uf.FollowerId == followerId && uf.FolloweeId == followeeId);
+        }
     }
 }
